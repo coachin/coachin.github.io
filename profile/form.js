c1d99546ead7dd0721f621bc64ddb800f2eeb976
Chirag <csanghvi@linkedin.com>
2017-07-15 09:36:34 -0700

    fixing redirect issues and string change

--- profile/form.js
+++ profile/form.js
@@ -13,11 +13,13 @@ formApp.controller('formController', ['$scope', '$http', 'socialLoginService', f
 
     $scope.updateData =  function() {
     	var id = $scope.user._id;
-    	delete $scope.user._id;
+        delete $scope.user._id;
+        var exists = $scope.user.exists;
+        $scope.user.exists = true;
         $http.post('http://tutorin-ghci.rhcloud.com/post/user/' + id, $scope.user).then(function(response) {
         	$scope.user._id = id;
             window.localStorage.setItem('coachInUser', JSON.stringify($scope.user));
-            if ($scope.user.exists == true) {
+            if (exists == true || !$scope.user.isTutor) {
                 window.location = '/index.html';
             } else {
                 window.location = '/preference/index.html';
